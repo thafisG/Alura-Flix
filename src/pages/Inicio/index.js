79f79412538382a4d1f1e3983a4655d8a4c7f619
MonicaHillman <mmhillman1@ucs.br>
2022-11-11 14:08:24 -0300

    api externa

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,20 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json';
+import { useEffect, useState } from "react";
 import styles from './Inicio.module.css';
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
             <Banner imagem="home" />
